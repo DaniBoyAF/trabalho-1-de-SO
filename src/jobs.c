b8ad474287ce4568d039e704d14a6de4d02cba1c
Daniel andrade <moongames12345@gmail.com>
2026-08-23 21:17:59 -0300

    correção
    fui testar e não achei o erro e tive q usar ia para corrigi não criar o codigo

---
+++ src/jobs.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct{
+    int id;
+    pid_t pid;
+    int ativo;
+}Job;
--- src/main.c
+++ src/main.c
@@ -1,76 +1,92 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "processflow.h"
-#include "task.c"
 
 int main(void){
 
     ProcessInfo processo;
     char nome[3000];
-    int resultado = adicionar_tarefa(&processo);
-    if(resultado == 1){
-        printf("Tarefa adicionada com sucesso!\n");
-    }else{
-        printf("Erro ao adicionar tarefa!\n");
-    }
+
     while(1)
     {
-     printf("processflow> ");
-     fgets(nome, sizeof(nome), stdin);
+        printf("processflow> ");
 
-     nome[strcspn(nome, "\n")] = 0;
-     if(strlen(nome)== 0){
-        continue;
-     }
-     char *token;
-     token = strtok(nome, " ");
-      printf("Você digitou: %s\n", token);
-     token = strtok(NULL, " ");
-      printf("Você digitou: %s\n", token);
-     token = strtok(NULL, " ");
-      printf("Você digitou: %s\n", token);
-
-     if(strcmp(nome,"help")==0){
-        printf("Comandos disponíveis:\n");
-        printf("help - Mostra os comandos disponíveis\n");
-        printf("exit - Sai do programa\n");
-        printf("Qualquer outro comando será apenas impresso na tela\n");
-     }
-
-
-     if(strcmp(nome,"task")== 0)
-     {
-        printf("Comando task encontrado !\n");
-        continue;
-     }
-      
-      token = strtok(NULL, " ");
-      if(token == NULL){
-        printf("erro: informe o nome da tarefa\n");
-        continue;
-      }
-    processo.arg[0] = '\0';
-      token = strtok(NULL, " ");
-      while(token != NULL){
-        printf("Argumento: %s\n", token);
-        strcat(processo.arg, token);
-        strcat(processo.arg, " ");
-        process.num_args++;
+        fgets(nome, sizeof(nome), stdin);
+        nome[strcspn(nome, "\n")] = 0;
+        if(strlen(nome) == 0){
+            continue;
+        }
+        char *token;
+        token = strtok(nome, " ");
+        printf("Você digitou: %s\n", token);
         token = strtok(NULL, " ");
-      }
-      ProcessInfo processo ;
-      strcpy(processo.name, token);
-      printf("Nome: %s\n", processo.name);
-      printf("Programa: %s\n", processo.programa);
-      printf("Quantidade de Argumentos: %d\n", processo.num_args);
-      continue;
 
+        if(token != NULL){
+            printf("Você digitou: %s\n", token);
+        }
+        token = strtok(NULL, " ");
+        if(token != NULL){
+            printf("Você digitou: %s\n", token);
+        }
+        if(token != NULL && strcmp(token, "input") == 0)
+        {
+            printf("Comando input encontrado !\n");
+            continue;
+        }
+        if(token != NULL && strcmp(token, "run") == 0)
+        {
+            printf("Comando run encontrado !\n");
+            continue;
+        }
+        if(strcmp(nome, "help") == 0){
+            printf("Comandos disponíveis:\n");
+            printf("help - Mostra os comandos disponíveis\n");
+            printf("exit - Sai do programa\n");
+            printf("Qualquer outro comando será apenas impresso na tela\n");
+
+            continue;
+        }
+        if(strcmp(nome, "task") == 0)
+        {
+            printf("Comando task encontrado !\n");
+            continue;
+        }
+        processo.arg[0] = '\0';
+        processo.quantidade_args = 0;
+        token = strtok(NULL, " ");
+        if(token == NULL){
+            printf("erro: informe o nome da tarefa\n");
+            continue;
+        }
+        while(token != NULL){
+
+            printf("Argumento: %s\n", token);
+
+            strcat(processo.arg, token);
+            strcat(processo.arg, " ");
+
+            processo.quantidade_args++;
+
+            token = strtok(NULL, " ");
+        }
+
+        printf("Nome: %s\n", processo.nome);
+        printf("Programa: %s\n", processo.programa);
+        printf("Quantidade de Argumentos: %d\n",
+               processo.quantidade_args);
+
+        if(token != NULL && strcmp(token, "workdir") == 0)
+        {
+            token = strtok(NULL, " ");
+        }
+        if(strcmp(nome, "exit") == 0){
+            return 0;
+        }
+
+
+        printf("Você escreveu isso aqui: %s\n", nome);
     }
- 
-     if(strcmp(nome,"exit")==0){
-        return 0;
-     }
-    
-     printf("Você escreveu isso aqui: %s",nome);
-    }
+
+    return 0;
 }
